c8f3027cf032eb63c87db9e346560bbccf55d4e8
zhyarali <zhyaralimahmud@gmail.com>
2022-10-27 10:51:07 +0300

    add all

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+import {createApp } from 'vue';
+import App from './App.vue';
+
+import router from './router/index.js'
+
+createApp(App).use(router)
+.mount("#app")
---
+++ resources/js/pages/home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button @click="add_counter">counter by one</button>
+        <p>{{counter}}</p>
+    </div>
+</template>
+
+<script>
+import {ref} from 'vue';
+export default {
+    setup () {
+
+        const counter=ref(0);
+
+        function add_counter() {
+            counter.value+=1;
+        }
+
+        return {counter,add_counter}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
